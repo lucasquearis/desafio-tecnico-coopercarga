2d97b9fe4d162b08b43193a4062733a776534222
lucasquearis <lucasquearis@hotmail.com>
2023-08-25 13:09:55 -0300

    feat: configura docker

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 9000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
 });
